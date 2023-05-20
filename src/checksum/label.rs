4170d0bc367cf351f42abf09e109e5d7d2cd9d95
Jan Kasiak <jan@cybojanek.net>
2023-05-20 17:50:18 +0200

    Change checksum::label to use phys::ChecksumTail

--- src/checksum/label.rs
+++ src/checksum/label.rs
@@ -1,10 +1,10 @@
 /*! Label checksum.
  *
- * * The `label` checksum is embedded at the tail end of a block.
- * * It uses `sha256`, where the checksum endian encoding is specified using the magic.
- * * It is used to checksum blocks in the label (boot block, nv list, uber blocks).
- * * The checksum is calculated over the entire block (including the tail).
- * * When calculating the checksum, `checksum 0` is set to the `offset` of the block,
+ * - The `label` checksum is embedded at the tail end of a block.
+ * - It uses `sha256`, where the checksum endian encoding is specified using the magic.
+ * - It is used to checksum blocks in the label (boot block, nv list, uber blocks).
+ * - The checksum is calculated over the entire block (including the tail).
+ * - When calculating the checksum, `checksum 0` is set to the `offset` of the block,
  *   and `checksum 1`, `checksum 2`, `checksum 3` are all set to `0`.
  *
  * Embedded at tail of data.
@@ -37,23 +37,15 @@ use core::result::Result::{Err, Ok};
 use std::error;
 
 use crate::checksum::{sha_256_digest, sha_256_digest_slices};
-use crate::endian;
-use crate::endian::Endian;
-
-////////////////////////////////////////////////////////////////////////////////
-
-/// Byte length of an encoded label checksum (including magic).
-pub const LENGTH: usize = 40;
-
-/// Embedded checksum magic.
-pub const MAGIC: u64 = 0x210da7ab10c7a11;
+use crate::endian::{DecodeError, EncodeError, Encoder, Endian};
+use crate::phys::{ChecksumTail, ChecksumValue};
 
 ////////////////////////////////////////////////////////////////////////////////
 
 /** Compute the checksum of the `data` block and encode it at the end of `data`.
  *
- * * `offset` is included in checksum computation
- * * `endian` specifies the checksum encoding
+ * - `offset` is included in checksum computation
+ * - `endian` specifies the checksum encoding
  *
  * # Errors
  *
@@ -66,44 +58,37 @@ pub fn label_checksum(
 ) -> Result<(), LabelChecksumError> {
     // Check length.
     let length = data.len();
-    if length < LENGTH {
+    if length < ChecksumTail::LENGTH {
         return Err(LabelChecksumError::InvalidLength { length: length });
     }
 
-    // Grab tail (including magic).
-    let tail = &mut data[length - LENGTH..length];
-
-    // Create encoder.
-    let mut encoder = endian::Encoder::to_bytes(tail, endian);
-
-    // Encode MAGIC, offset, and zeroes.
-    encoder.put_u64(MAGIC)?;
-    encoder.put_u64(offset)?;
-    encoder.put_u64(0)?;
-    encoder.put_u64(0)?;
-    encoder.put_u64(0)?;
+    // Encode tail.
+    let tail = ChecksumTail {
+        endian: endian,
+        value: ChecksumValue {
+            words: [offset, 0, 0, 0],
+        },
+    };
+    let tail_bytes = &mut data[length - ChecksumTail::LENGTH..length];
+    tail.to_bytes(tail_bytes.try_into().unwrap())?;
 
     // Compute checksum.
     let checksum = sha_256_digest(data);
 
-    // Grab tail (excluding magic).
-    let tail = &mut data[length - 32..length];
-
-    // Create encoder.
-    let mut encoder = endian::Encoder::to_bytes(tail, endian);
-
-    // Encode checksum.
-    encoder.put_u64(checksum[0])?;
-    encoder.put_u64(checksum[1])?;
-    encoder.put_u64(checksum[2])?;
-    encoder.put_u64(checksum[3])?;
+    // Encode tail.
+    let tail = ChecksumTail {
+        endian: endian,
+        value: ChecksumValue { words: checksum },
+    };
+    let tail_bytes = &mut data[length - ChecksumTail::LENGTH..length];
+    tail.to_bytes(tail_bytes.try_into().unwrap())?;
 
     Ok(())
 }
 
 /** Verify the checksum of the `data` block.
  *
- * * `offset` is included in checksum computation
+ * - `offset` is included in checksum computation
  *
  * # Errors
  *
@@ -113,16 +98,16 @@ pub fn label_checksum(
 pub fn label_verify(data: &[u8], offset: u64) -> Result<(), LabelVerifyError> {
     // Check length.
     let length = data.len();
-    if length < LENGTH {
+    if length < ChecksumTail::LENGTH {
         return Err(LabelVerifyError::InvalidLength { length: length });
     }
 
-    // Grab tail (including magic).
-    let tail = &data[length - LENGTH..length];
-    let mut decoder = match endian::Decoder::from_u64_magic(tail, MAGIC) {
+    // Decode ChecksumTail.
+    let tail = &data[length - ChecksumTail::LENGTH..length];
+    let tail = match ChecksumTail::from_bytes(tail.try_into().unwrap()) {
         Ok(v) => v,
         Err(e) => match e {
-            endian::DecodeError::InvalidMagic {
+            DecodeError::InvalidMagic {
                 expected: _,
                 actual,
             } => {
@@ -137,19 +122,11 @@ pub fn label_verify(data: &[u8], offset: u64) -> Result<(), LabelVerifyError> {
         },
     };
 
-    // Decode checksum.
-    let data_checksum = [
-        decoder.get_u64()?,
-        decoder.get_u64()?,
-        decoder.get_u64()?,
-        decoder.get_u64()?,
-    ];
-
     // Create offset checksum.
     let offset_checksum = &mut [0; 32];
 
     // Create encoder.
-    let mut encoder = endian::Encoder::to_bytes(offset_checksum, decoder.endian());
+    let mut encoder = Encoder::to_bytes(offset_checksum, tail.endian);
 
     // Encode offset and zeroes.
     encoder.put_u64(offset)?;
@@ -162,7 +139,7 @@ pub fn label_verify(data: &[u8], offset: u64) -> Result<(), LabelVerifyError> {
     let computed_checksum = sha_256_digest_slices(slices);
 
     // Compare checksum.
-    if data_checksum == computed_checksum {
+    if tail.value.words == computed_checksum {
         Ok(())
     } else {
         Err(LabelVerifyError::Mismatch {})
@@ -173,21 +150,21 @@ pub fn label_verify(data: &[u8], offset: u64) -> Result<(), LabelVerifyError> {
 
 #[derive(Debug)]
 pub enum LabelChecksumError {
-    /** Endian pack error:
+    /** Endian pack error.
      *
-     * * `err` - [`endian::DecodeError`]
+     * - `err` - [`DecodeError`]
      */
-    EndianEncodeError { err: endian::EncodeError },
+    EndianEncodeError { err: EncodeError },
 
     /** Invalid length.
      *
-     * * `length` - Length of data.
+     * - `length` - Length of data.
      */
     InvalidLength { length: usize },
 }
 
-impl From<endian::EncodeError> for LabelChecksumError {
-    fn from(value: endian::EncodeError) -> Self {
+impl From<EncodeError> for LabelChecksumError {
+    fn from(value: EncodeError) -> Self {
         LabelChecksumError::EndianEncodeError { err: value }
     }
 }
@@ -199,10 +176,7 @@ impl fmt::Display for LabelChecksumError {
                 write!(f, "Label checksum Endian pack error: {err}")
             }
             LabelChecksumError::InvalidLength { length } => {
-                write!(
-                    f,
-                    "Label checksum error: invalid length {length} expected at least {LENGTH}"
-                )
+                write!(f, "Label checksum error: invalid length {length}")
             }
         }
     }
@@ -226,21 +200,21 @@ pub enum LabelVerifyError {
      */
     EmptyMagic {},
 
-    /** Endian pack error:
+    /** Endian pack error.
      *
-     * * `err` - [`endian::DecodeError`]
+     * - `err` - [`DecodeError`]
      */
-    EndianEncodeError { err: endian::EncodeError },
+    EndianEncodeError { err: EncodeError },
 
-    /** Endian unpack error:
+    /** Endian unpack error.
      *
-     * * `err` - [`endian::DecodeError`]
+     * - `err` - [`DecodeError`]
      */
-    EndianDecodeError { err: endian::DecodeError },
+    EndianDecodeError { err: DecodeError },
 
     /** Invalid length.
      *
-     * * `length` - Length of data.
+     * - `length` - Length of data.
      */
     InvalidLength { length: usize },
 
@@ -248,14 +222,14 @@ pub enum LabelVerifyError {
     Mismatch {},
 }
 
-impl From<endian::EncodeError> for LabelVerifyError {
-    fn from(value: endian::EncodeError) -> Self {
+impl From<EncodeError> for LabelVerifyError {
+    fn from(value: EncodeError) -> Self {
         LabelVerifyError::EndianEncodeError { err: value }
     }
 }
 
-impl From<endian::DecodeError> for LabelVerifyError {
-    fn from(value: endian::DecodeError) -> Self {
+impl From<DecodeError> for LabelVerifyError {
+    fn from(value: DecodeError) -> Self {
         LabelVerifyError::EndianDecodeError { err: value }
     }
 }
@@ -273,10 +247,7 @@ impl fmt::Display for LabelVerifyError {
                 write!(f, "Label verify Endian unpack error: {err}")
             }
             LabelVerifyError::InvalidLength { length } => {
-                write!(
-                    f,
-                    "Label verify error: invalid length {length} expected at least {LENGTH}"
-                )
+                write!(f, "Label verify error: invalid length {length}")
             }
             LabelVerifyError::Mismatch {} => write!(f, "Label verify checksum mismatch"),
         }
